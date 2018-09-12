f16c12a7fd9425d21744283ac118f37627e7c3db
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-09-12 11:43:10 +0200

    Añadido dbprueba.sql para crear la tabla categoria

---
+++ dbprueba.sql
@@ -0,0 +1,8 @@
+create table categoria (
+  id serial primary key,
+  nombre varchar(50) not null unique
+);
+
+insert into categoria (nombre) values ('categoría 1');
+insert into categoria (nombre) values ('categoría 2');
+insert into categoria (nombre) values ('categoría 3');
