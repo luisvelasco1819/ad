2c9f272c56af5d3bac8c6c29d4ff9ea8059df823
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-09-14 10:27:29 +0200

    CMySql implementada lectura de la tabla categoria

--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace CMySql
 {
     class MainClass
     {
+		private static string[] getFieldNames() {
+			throw new NotImplementedException();
+		}
+
         public static void Main(string[] args)
         {
-			MySqlConnection mySqlConnection = new MySqlConnection(
+			IDbConnection dbConnection = new MySqlConnection(
 				"server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
 			);
-			mySqlConnection.Open();
-			mySqlConnection.Close();
+			dbConnection.Open();
+            
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+			dbCommand.CommandText = "select * from categoria order by id";
+			IDataReader dataReader = dbCommand.ExecuteReader();
+			while (dataReader.Read()) 
+				Console.WriteLine("id='{0}' nombre='{1}'", dataReader["id"], dataReader["nombre"]);
+    			//Console.WriteLine("id={0} nombre={1}", dataReader[0], dataReader[1]);
+            dataReader.Close();
+
+			dbConnection.Close();
         }
     }
 }
