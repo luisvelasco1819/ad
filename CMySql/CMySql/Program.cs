d7f06755d62f2bedc0db028b88198995ca2b352e
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-09-18 09:54:47 +0200

    CMySql resuelto getFielNames

--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,14 +1,24 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Data;
+using System.Collections.Generic;
 
 namespace CMySql
 {
     class MainClass
     {
-		private static string[] getFieldNames() {
-			throw new NotImplementedException();
-		}
+		private static string[] getFieldNames(IDataReader dataReader) {
+			//int fieldCount = dataReader.FieldCount;
+			//string[] fieldNames = new string[fieldCount];
+			//for (int index = 0; index < fieldCount; index++)
+			//	fieldNames[index] = dataReader.GetName(index);
+			//return fieldNames;
+			int fieldCount = dataReader.FieldCount;
+			List<string> fieldNameList = new List<string>();
+			for (int index = 0; index < fieldCount; index++)
+				fieldNameList.Add(dataReader.GetName(index));
+			return fieldNameList.ToArray();
+   		}
 
         public static void Main(string[] args)
         {
@@ -18,11 +28,15 @@ namespace CMySql
 			dbConnection.Open();
             
             IDbCommand dbCommand = dbConnection.CreateCommand();
-			dbCommand.CommandText = "select * from categoria order by id";
+			dbCommand.CommandText = "select id, nombre from categoria order by id";
 			IDataReader dataReader = dbCommand.ExecuteReader();
-			while (dataReader.Read()) 
-				Console.WriteLine("id='{0}' nombre='{1}'", dataReader["id"], dataReader["nombre"]);
-    			//Console.WriteLine("id={0} nombre={1}", dataReader[0], dataReader[1]);
+			string[] fieldNames = getFieldNames(dataReader);
+			foreach (string fieldName in fieldNames)
+				Console.WriteLine("columna=" + fieldName);
+
+			//while (dataReader.Read()) 
+				//Console.WriteLine("id='{0}' nombre='{1}'", dataReader["id"], dataReader["nombre"]);
+    			////Console.WriteLine("id={0} nombre={1}", dataReader[0], dataReader[1]);
             dataReader.Close();
 
 			dbConnection.Close();
