a9ac34e930e2f169bb4d8ce770c799bddba9a2e8
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-09-21 10:50:40 +0200

    CCategoria modificado usando delegates y Reflection

--- CCategoria/CCategoria/Categoria.cs
+++ CCategoria/CCategoria/Categoria.cs
@@ -3,8 +3,25 @@ namespace CCategoria
 {
     public class Categoria
     {
-        public Categoria()
-        {
+		private ulong id;
+		private string nombre;
+
+		public Categoria() {
         }
-    }
+
+		public Categoria(ulong id, string nombre) {
+			this.id = id;
+			this.nombre = nombre;
+        }
+
+		public ulong Id {
+			get { return id; }
+			set { id = value; }
+		}
+
+		public string Nombre {
+			get { return nombre; }
+			set { nombre = value; }
+		}
+	}
 }
--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -3,6 +3,9 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Data;
 
+using CCategoria;
+
+using System.Reflection;
 
 public partial class MainWindow : Gtk.Window
 {
@@ -15,13 +18,56 @@ public partial class MainWindow : Gtk.Window
                 "server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
         );
         dbConnection.Open();
-            
 
-		treeView.AppendColumn("ID", new CellRendererText(), "text", 0);
-		treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+
+		//treeView.AppendColumn("ID", new CellRendererText(), "text", 0);
+		//treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+		CellRendererText cellRendererText = new CellRendererText();
+		//treeView.AppendColumn(
+		//	"ID",
+		//	cellRendererText,
+		//	delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+		//		//Categoria categoria = (Categoria)tree_model.GetValue(iter, 0);
+		//		//cellRendererText.Text = categoria.Id + "";
+		//		object model = tree_model.GetValue(iter, 0);
+		//		object value = model.GetType().GetProperty("Id").GetValue(model);
+		//		cellRendererText.Text = value + "";
+		//    }
+		//);
+
+		//treeView.AppendColumn(
+    //        "Nombre",
+    //        cellRendererText,
+    //        delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+			 ////   Categoria categoria = (Categoria)tree_model.GetValue(iter, 0);
+				////cellRendererText.Text = categoria.Nombre + "";
+    			//object model = tree_model.GetValue(iter, 0);
+        //        object value = model.GetType().GetProperty("Nombre").GetValue(model);
+        //        cellRendererText.Text = value + "";
+        //    }
+        //);
+
+		string[] properties = new string[] { "Id", "Nombre" };
+
+		foreach(string property in properties) {
+			string propertyName = property;
+			treeView.AppendColumn(
+				property,
+                cellRendererText,
+                delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+                //Categoria categoria = (Categoria)tree_model.GetValue(iter, 0);
+                //cellRendererText.Text = categoria.Id + "";
+                object model = tree_model.GetValue(iter, 0);
+				object value = model.GetType().GetProperty(property).GetValue(model);
+                    cellRendererText.Text = value + "";
+                }
+            );
+		}
+
 
 		//ListStore listStore = new ListStore(typeof(ulong), typeof(string));
-		ListStore listStore = new ListStore(typeof(string), typeof(string));
+		//ListStore listStore = new ListStore(typeof(string), typeof(string));
+		ListStore listStore = new ListStore(typeof(object));
 		treeView.Model = listStore;
 
 		//listStore.AppendValues("1", "cat 1");
@@ -31,12 +77,12 @@ public partial class MainWindow : Gtk.Window
 		dbCommand.CommandText = "select id, nombre from categoria order by id";
 		IDataReader dataReader = dbCommand.ExecuteReader();
 		while (dataReader.Read())
-			listStore.AppendValues(dataReader["id"] + "", dataReader["nombre"] + "");
+			listStore.AppendValues(new Categoria((ulong)dataReader["id"], (string)dataReader["nombre"]));
 		dataReader.Close();
 
 
 		dbConnection.Close();
-
+        
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
