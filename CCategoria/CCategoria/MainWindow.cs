4eca7977ddb09ef14590a5f78299059f4b33e5a0
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-09-25 09:17:43 +0200

    CCategoria: pruebas de insert update y delete

--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -9,69 +9,38 @@ using System.Reflection;
 
 public partial class MainWindow : Gtk.Window
 {
-	
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
-        Build();
+	private IDbConnection dbConnection;
+	public MainWindow() : base(Gtk.WindowType.Toplevel) {
+		Build();
 
-		IDbConnection dbConnection = new MySqlConnection(
-                "server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
-        );
-        dbConnection.Open();
+		dbConnection = new MySqlConnection(
+				"server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
+		);
+		dbConnection.Open();
 
+		//insert();
+		//update();
+		update(new Categoria(3, "categoría 3 " + DateTime.Now));
+		//delete();
 
-		//treeView.AppendColumn("ID", new CellRendererText(), "text", 0);
-		//treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
 		CellRendererText cellRendererText = new CellRendererText();
-		//treeView.AppendColumn(
-		//	"ID",
-		//	cellRendererText,
-		//	delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
-		//		//Categoria categoria = (Categoria)tree_model.GetValue(iter, 0);
-		//		//cellRendererText.Text = categoria.Id + "";
-		//		object model = tree_model.GetValue(iter, 0);
-		//		object value = model.GetType().GetProperty("Id").GetValue(model);
-		//		cellRendererText.Text = value + "";
-		//    }
-		//);
-
-		//treeView.AppendColumn(
-    //        "Nombre",
-    //        cellRendererText,
-    //        delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
-			 ////   Categoria categoria = (Categoria)tree_model.GetValue(iter, 0);
-				////cellRendererText.Text = categoria.Nombre + "";
-    			//object model = tree_model.GetValue(iter, 0);
-        //        object value = model.GetType().GetProperty("Nombre").GetValue(model);
-        //        cellRendererText.Text = value + "";
-        //    }
-        //);
 
 		string[] properties = new string[] { "Id", "Nombre" };
 
-		foreach(string property in properties) {
-			string propertyName = property;
+		foreach (string property in properties){
 			treeView.AppendColumn(
 				property,
-                cellRendererText,
-                delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
-                //Categoria categoria = (Categoria)tree_model.GetValue(iter, 0);
-                //cellRendererText.Text = categoria.Id + "";
-                object model = tree_model.GetValue(iter, 0);
-				object value = model.GetType().GetProperty(property).GetValue(model);
-                    cellRendererText.Text = value + "";
-                }
-            );
-		}
+				cellRendererText,
+				delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+					object model = tree_model.GetValue(iter, 0);
+				    object value = model.GetType().GetProperty(property).GetValue(model);
+					cellRendererText.Text = value + "";
+				}
+			);
+	    }
 
-
-		//ListStore listStore = new ListStore(typeof(ulong), typeof(string));
-		//ListStore listStore = new ListStore(typeof(string), typeof(string));
 		ListStore listStore = new ListStore(typeof(object));
 		treeView.Model = listStore;
-
-		//listStore.AppendValues("1", "cat 1");
-		//listStore.AppendValues("2", "cat 2");
 
 		IDbCommand dbCommand = dbConnection.CreateCommand();
 		dbCommand.CommandText = "select id, nombre from categoria order by id";
@@ -80,13 +49,46 @@ public partial class MainWindow : Gtk.Window
 			listStore.AppendValues(new Categoria((ulong)dataReader["id"], (string)dataReader["nombre"]));
 		dataReader.Close();
 
-
 		dbConnection.Close();
-        
     }
 
-    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-    {
+	private void insert() {
+		IDbCommand dbCommand = dbConnection.CreateCommand();
+		dbCommand.CommandText = "insert into categoria (nombre) values ('categoría 4')";
+		int filas = dbCommand.ExecuteNonQuery();
+	}
+
+	private void update() {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = "update categoria set nombre='categoría 4 modificada' where id=4";
+        dbCommand.ExecuteNonQuery();
+    }
+
+    
+	private void update(Categoria categoria) {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+		dbCommand.CommandText = "update categoria set nombre=@nombre where id=@id";
+
+		IDbDataParameter dbDataParameterNombre = dbCommand.CreateParameter();
+		dbDataParameterNombre.ParameterName = "nombre";
+		dbDataParameterNombre.Value = categoria.Nombre;
+		dbCommand.Parameters.Add(dbDataParameterNombre);
+
+		IDbDataParameter dbDataParameterId = dbCommand.CreateParameter();
+        dbDataParameterId.ParameterName = "id";
+		dbDataParameterId.Value = categoria.Id;
+        dbCommand.Parameters.Add(dbDataParameterId);
+
+		dbCommand.ExecuteNonQuery();
+    }
+    
+	private void delete() {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = "delete from categoria where id=4";
+        dbCommand.ExecuteNonQuery();
+    }
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a) {
         Application.Quit();
         a.RetVal = true;
     }
