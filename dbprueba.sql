8fb60a3e057ffd4ba3c8aac70871f6845692f3dc
luisvelasco1819 <luis.velasco@institutoserpis.org>
2019-01-18 08:04:19 +0100

    dbprueba.sql: añadidas cliente, pedido y pedidolinea

--- dbprueba.sql
+++ dbprueba.sql
@@ -21,3 +21,34 @@ insert into articulo (nombre, precio, categoria) values ('artículo 2', 2.0, 2);
 insert into articulo (nombre, precio, categoria) values ('artículo 3', 3.0, 3);
 insert into articulo (nombre, precio) values ('artículo 4', 4.0);
 
+create table cliente (
+  id serial primary key,
+  nombre varchar(50) not null unique
+);
+
+insert into cliente (nombre) values ('cliente 1');
+insert into cliente (nombre) values ('cliente 2');
+insert into cliente (nombre) values ('cliente 3');
+
+create table pedido (
+  id serial primary key,
+  fecha datetime not null unique,
+  cliente bigint unsigned not null,
+  importe numeric(10,2) not null
+);
+
+alter table pedido add foreign key (cliente) references cliente (id);
+
+create table pedidolinea (
+  id serial primary key,
+  pedido bigint unsigned not null,
+  articulo bigint unsigned not null,
+  precio numeric(10,2) not null,
+  unidades numeric(10,2) not null,
+  importe numeric(10,2) not null
+);
+
+alter table pedidolinea add foreign key (pedido) references pedido (id) on delete cascade;
+alter table pedidolinea add foreign key (articulo) references articulo (id);
+
+
