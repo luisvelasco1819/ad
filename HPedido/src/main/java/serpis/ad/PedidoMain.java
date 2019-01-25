1feb8b2a7e996e0e39246fa756161d65f761662d
luisvelasco1819 <luis.velasco@institutoserpis.org>
2019-01-25 10:38:28 +0100

    HPedido usando remove

--- HPedido/src/main/java/serpis/ad/PedidoMain.java
+++ HPedido/src/main/java/serpis/ad/PedidoMain.java
@@ -1,27 +1,62 @@
 package serpis.ad;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class PedidoMain {
+	private static EntityManagerFactory entityManagerFactory;
 
 	public static void main(String[] args) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("serpis.ad.hmysql");
+		entityManagerFactory = Persistence.createEntityManagerFactory("serpis.ad.hmysql");
 		
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
-		Articulo articulo = entityManager.find(Articulo.class, 1L);
+		List<Categoria> categorias = 
+				entityManager.createQuery("select c from Categoria c", Categoria.class).getResultList();
+		
+		Articulo articulo = newArticulo();
+		articulo.setCategoria(categorias.get( new Random().nextInt(categorias.size()) ));
+		
+		entityManager.persist(articulo);
+		
 		show(articulo);
 		
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		
+		System.out.print("Añadido articulo. Pulsa Enter para seguir...");
+		new Scanner(System.in).nextLine();
+		
+		remove(articulo);
+		
 		entityManagerFactory.close();
+	}
+	
+	private static Articulo newArticulo() {
+		Articulo articulo = new Articulo();
+		articulo.setNombre("nuevo " + LocalDateTime.now());
+		articulo.setPrecio(new BigDecimal(1.5));
+		return articulo;
+	}
+	
+	private static void remove(Articulo articulo) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		//articulo = entityManager.find(Articulo.class, articulo.getId());
+		articulo = entityManager.getReference(Articulo.class, articulo.getId());
+		entityManager.remove(articulo);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 	
 	private static void show(Articulo articulo) {
